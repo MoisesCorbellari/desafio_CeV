a8ec9d4ba4623e73461d01099e372c01745b423b
Moises Corbellari <moisescorbellari@outlook.com>
2024-07-23 23:57:56 -0300

    refazedo

---
+++ Mundo 3/Desafio_85 - Listas com pares e ímpares.py
@@ -0,0 +1,25 @@
+'''
+Crie um programa onde o usuário possa digitar sete valores numéricos e 
+cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. 
+No final, mostre os valores pares e ímpares em ordem crescente.
+'''
+
+pares = []
+impares = []
+
+def line(txt, width=60):
+    prompt = f'\n{txt:^{width}}'
+    print(prompt)
+    print('-'*width)
+
+for p in range(0,7):
+    num = int(input(f'Digite o {p+1}º valor: '))
+    if num % 2 == 0:
+        pares.append(num)
+    else:
+        impares.append(num)
+pares.sort()
+impares.sort()
+line('Lista com pares e impares', 60)
+print(f'\n-> Valores pares digitados: {pares}')
+print(f'-> Valores ímpares digitados: {impares}\n')
